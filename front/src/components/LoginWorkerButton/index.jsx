326d09b67202d30ff9a15819cf0368154dc4baaa
n0izynator <maciejewski.konta@gmail.com>
2020-06-16 15:40:24 +0200

    Dodałem formularz logowania pracownika i formularz zgłoszenia klienta

---
+++ front/src/components/LoginWorkerButton/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginWorkerButton = props => {
+    return <button onClick={ props.loginWorker } className="waves-effect waves-light btn btn-size">Zaloguj jako pracownik</button>
+}
+
+export default LoginWorkerButton
