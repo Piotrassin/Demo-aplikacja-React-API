9ba0dc60e53a2b1a73d85ca7f83e3bbe1f301b4c
n0izynator <maciejewski.konta@gmail.com>
2020-06-16 23:34:27 +0200

    added

---
+++ front/src/components/MyOffersButton/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddApplicationButton = props => {
+    return <button onClick={ props.addAplication } className="waves-effect waves-light btn btn-size">Zapisz się na wymarzony lot!</button>
+}
+
+export default AddApplicationButton
---
+++ front/src/components/RequestOfferListButton/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RequestListButton = props => {
+    return <button onClick={ props.addAplication } className="waves-effect waves-light btn btn-size">Zgłoszenia oczeukjące</button>
+}
+
+export default RequestListButton
