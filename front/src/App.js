dc39dbbdc8fe0d8e660fa850ccf952b80ed23197
n0izynator <maciejewski.konta@gmail.com>
2020-06-16 13:06:44 +0200

    Ekran startowy aplikacji

--- front/src/App.js
+++ front/src/App.js
@@ -6,19 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SpaceY</h1>
       </header>
+      <div className="buttons">
+          <button className="waves-effect waves-light btn btn-size">Zapisz się na wymarzony lot!</button>
+          <button className="waves-effect waves-light btn btn-size">Zaloguj jako pracownik</button>
+      </div>
+
+
     </div>
   );
 }
