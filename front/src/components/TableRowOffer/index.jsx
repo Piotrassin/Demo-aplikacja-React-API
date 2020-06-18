daf1d8bf1a4b5b9a122069783e88e7775bbc7947
n0izynator <maciejewski.konta@gmail.com>
2020-06-18 15:11:12 +0200

    dodałem liste ofert

---
+++ front/src/components/TableRowOffer/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+export default function TableRow({ person }) {
+
+
+    return (
+        <tr style={{ color: "black" }} >
+            <td>{person.id}</td>
+            <td>{person.firstName}</td>
+            <td>{person.lastName}</td>
+            <td>{person.email}</td>
+            <button type="button" className="btn green darken-4" style={{ marginTop: "3%" }} >Zaakceptuj</button>
+        </tr>
+    );
+}
